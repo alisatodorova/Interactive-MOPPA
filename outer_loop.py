69dde0e0cbcbf2722da5e3181de23faae7cfaf1e
alisatodorova <alisa.todorova@gmail.com>
2023-05-23 16:08:08 +0200

    Outer-loop

--- outer_loop.py
+++ outer_loop.py
@@ -8,12 +8,13 @@ import networkx as nx
 import gaussian_process
 import acquisition_function
 
-def outer(G, S, T):
+def outer(G, S, T, d):
     """
     Selects the target direction
     :param G: Multi-objective search graph G = (V, E)
     :param S: Starting node
     :param T: Terminating (ending) node
+    :param d: Objectives
     :return Recommended path p and its value (cost) v_p
     """
 
@@ -27,15 +28,17 @@ def outer(G, S, T):
     #TODO: import/initialise GP and acquisition function
     gp = gaussian_process.GPPairwise(num_objectives=2)
     af = acquisition_function.DiscreteAcquirer()
-
     u = gp.initialise_gaussian_process()  # Initialise the Gaussian process
     u_hat = af.initialise_acquirer()  # Initialise acquisition function
 
     P = set() #Pareto set
 
     # Path initialisation
-    for i in objectives:
-        p = nx.shortest_path(G, source = i, method = 'dijkstra')
+    for i in d:
+        p = nx.shortest_path(G, source=S, target=T, weight = i, method='dijkstra')
+        obj_val = G.nodes[p[-1]][i] #Objectives' values where -1 retrieves the value of the ending node in the path
         P = P.add(p)
+
+    C = (min(obj_val,), min(obj_val)) #Candidate target
 
     return p, v_p
